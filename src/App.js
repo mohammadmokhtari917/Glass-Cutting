96767d5bcf675a094559c6f8f67d10792e9a8142
Mohammad Mokhtari <118061522+mohammadmokhtari917@users.noreply.github.com>
2022-12-17 00:11:44 -0800

    Creating homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './App.css';
+
 import Toolbar from './components/Toolbar/Toolbar';
-const App=() =>{
-  return (         
-            <div className="App"> 
-              <Toolbar />
-            </div>
+import HomePage from './components/Homepage/Homepage';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+
+      <div className="App">
+
+        <Toolbar />
+
+        <Routes>
+          <Route path="/" exact element={<HomePage />} />
+          {/* <Route path="/student/:studentid" exact element={<EditStudent />} /> */}
+          <Route render={() => <h1>not Found</h1>} />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Homepage.css';
+
+const HomePage = (props) => {
+
+  return (
+    <React.Fragment>
+      <div className='homepage'>
+        <h2>سلام</h2>
+      </div>
+    </React.Fragment>
+  )
+
+}
+export default HomePage;
