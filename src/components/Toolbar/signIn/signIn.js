908528b124e971c127f5c655f9c77eae47239508
Mohammad Mokhtari <118061522+mohammadmokhtari917@users.noreply.github.com>
2022-12-19 15:49:27 -0800

    Creating Captcha

--- src/components/Toolbar/signIn/signIn.js
+++ src/components/Toolbar/signIn/signIn.js
@@ -5,48 +5,48 @@ import reloadimage from '../../../assets/images/reload_image.jpg';
 import './signIn.css';
 import { AuthContext } from '../../context/Auth/authContext';
 const SignIn = (props) => {
-    // const [randomNumber1, setRandomNumber1] = useState(0);
-    // const [randomNumber2, setRandomNumber2] = useState(0);
-    // const [sumHolder, setSumHolder] = useState(0);
-    // const [captchaValue, setCaptchaValue] = useState(0);
+    const [randomNumber1, setRandomNumber1] = useState(0);
+    const [randomNumber2, setRandomNumber2] = useState(0);
+    const [sumHolder, setSumHolder] = useState(0);
+    const [captchaValue, setCaptchaValue] = useState(0);
     const [errorMessage, setErrorMessage] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     // const { dispatch } = useContext(AuthContext);
-    // useEffect(() => {
-    //     generateCaptcha();
-    // }, [])
-    // const generateCaptcha = () => {
-    //     let number1 = Math.floor(Math.random() * 10) + 1;
-    //     let number2 = Math.floor(Math.random() * 10) + 1;
-    //     setRandomNumber1(number1);
-    //     setRandomNumber2(number2);
-    //     let sum = number1 + number2;
-    //     setSumHolder(sum);
-    // }
-    // const cpatchaHandler = (event) => {
-    //     setCaptchaValue(Number(event.target.value));
-    // }
-    // const validate = () => {
-    //     if (username === '') {
-    //         setErrorMessage('username empty');
-    //         return false;
-    //     }
-    //     else if (!username.includes('@') || !username.includes('.')) {
-    //         setErrorMessage('username invalid');
-    //         return false;
-    //     }
-    //     else if (password === '') {
-    //         setErrorMessage('password empty');
-    //         return false;
-    //     }
-    //     else if (password.length < 5) {
-    //         setErrorMessage('password invalid');
-    //         return false;
-    //     }
-    //     setErrorMessage('');
-    //     return true;
-    // }
+    useEffect(() => {
+        generateCaptcha();
+    }, [])
+    const generateCaptcha = () => {
+        let number1 = Math.floor(Math.random() * 10) + 1;
+        let number2 = Math.floor(Math.random() * 10) + 1;
+        setRandomNumber1(number1);
+        setRandomNumber2(number2);
+        let sum = number1 + number2;
+        setSumHolder(sum);
+    }
+    const cpatchaHandler = (event) => {
+        setCaptchaValue(Number(event.target.value));
+    }
+    const validate = () => {
+        if (username === '') {
+            setErrorMessage('username empty');
+            return false;
+        }
+        else if (!username.includes('@') || !username.includes('.')) {
+            setErrorMessage('username invalid');
+            return false;
+        }
+        else if (password === '') {
+            setErrorMessage('password empty');
+            return false;
+        }
+        else if (password.length < 5) {
+            setErrorMessage('password invalid');
+            return false;
+        }
+        setErrorMessage('');
+        return true;
+    }
 
     const loginHandler = () => {
         if (sumHolder === captchaValue) {
@@ -92,11 +92,11 @@ const SignIn = (props) => {
             <p style={{ color: 'red', fontSize: '56' }}>{errorMessage}</p>
             <input type="text" placeholder="username" value={username} onChange={usernameHandler} />
             <input type="password" placeholder="password" value={password} onChange={passwordHandler} />
-            {/* <div className="captcha_view">
+            <div className="captcha_view">
                 <img src={reloadimage} alt="reloadimage" onClick={generateCaptcha} />
                 <input type="text" onChange={cpatchaHandler} />
                 <p>{randomNumber1} +{randomNumber2}=</p>
-            </div> */}
+            </div>
             <Button clicked={loginHandler}>ورود</Button>
         </React.Fragment>
     )
